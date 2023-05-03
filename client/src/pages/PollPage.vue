0afbbca7fa02734d673f9cb12a660afbefce7c4d
“Henning <henning@bronxsoftware.org>
2023-05-02 20:24:49 -0400

    updated the text on the poll page. added a scroll window for the job list

--- client/src/pages/PollPage.vue
+++ client/src/pages/PollPage.vue
@@ -1,23 +1,18 @@
 <template>
     <div>
-      <h3 class="gt-sm text-center">Job Titles in Demand in New York City 2023</h3>
-      <h5 class="lt-md text-center">Job Titles in Demand in New York City 2023</h5>
+      <h3 class="gt-sm text-center q-my-md">Pick your favorite job</h3>
+      <h5 class="lt-md text-center q-my-md">Pick your favorite job</h5>
       <div class="row">
         <div class="col-8" style="margin:auto;">
-          <q-card>
+          <q-card v-if="windowHeight > 700">
             <q-card-section>
-              <span class="text-bold">Instructions:</span> The table below shows the jobs most in demand by employers in
-              New York City.
-              Each entry lists the number of job openings, the number of employers with job openings, the estimated
-              average annual wage and the required education.
-              If you cannot find your perfect choice pick something that is most similar.
-              <br /><br />
-              You can sort the table in multiple ways by using the dropbox below.
-              <br /><br />
-              This is an anonymous poll. Your name will not be visible to others. You can change your choice at any time.
-              <br /><br />
-              <span class="text-bold">SAVE your choice:</span> Make a selection, then scroll down to the end of the table
-              to save your choice.
+              <p>Below is the list of {{ jobTitles.length }} in-demand jobs in New York City. Pick your favorite job.</p>
+              
+              <p><span class="text-bold">Your privacy:</span>
+              This is an anonymous poll. Your name will not be visible to others. You can change your choice at any time.</p>
+              
+              <p class="q-mb-none"><span class="text-bold">SAVE your choice: </span>Scroll through the list. Make a selection, then save your choice.</p>
+              
             </q-card-section>
           </q-card>
           <q-card class="q-mt-sm">
@@ -25,7 +20,7 @@
               <div class="q-py-md">
                 <q-select outlined v-model="sort" :options="options" label="Sort" />
               </div>
-              <q-list bordered separator>
+              <q-list bordered separator :style="cardHeight" class="overflow-auto">
                 <q-item v-for="(jobTitle, idx) in jobTitles" :key="idx">
                   <q-item-section avatar>
                     <q-radio v-model="selected" :val="jobTitle.JobTitle" />
@@ -46,7 +41,7 @@
             <q-separator dark />
             <q-card-actions align="around">
               <q-btn color="green" @click="saveChoice">Save your choice</q-btn>
-              <q-btn color="blue" :to="{ name: 'Results' }">Show poll results</q-btn>
+              <q-btn v-if="saved" color="blue" :to="{ name: 'Results' }">Show poll results</q-btn>
             </q-card-actions>
           </q-card>
         </div>
@@ -58,12 +53,21 @@
     data() {
       return {
         error: false,
+        saved: false,
         user: null,
         id: null,
         jobTitles: [],
         selected: null,
         options: ['title', 'jobs', 'employers', 'wage'],
         sort: 'title'
+      }
+    },
+    computed: {
+      windowHeight () {
+        return window.innerHeight
+      },
+      cardHeight () {
+        return window.innerHeight > 700 ? 'height: calc(100vh - 450px);' : 'height: calc(100vh - 270px);'
       }
     },
     watch: {
@@ -115,6 +119,7 @@
           body: JSON.stringify(data)
         })
           .then(() => {
+            this.saved = true
            this.$q.notify({
             message: 'Your choice has been recorded. Thank you!',
             color: 'positive'
